fb49d17ec5a16eb043f0ebc1e9d27e374361c7b3
Mike Weaver <miketweaver@users.noreply.github.com>
2017-05-18 11:23:05 -0600

    add stardew ponics and modupdater

---
+++ Stardewponics/Greenhouse.cs
@@ -0,0 +1,119 @@
+﻿using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewValley;
+using StardewModdingAPI;
+using StardewValley.Buildings;
+using StardewValley.Menus;
+
+namespace Stardewponics
+{
+    public class Greenhouse : Building
+    {
+		/*********
+		** Public methods
+		*********/
+
+        public Greenhouse(BluePrint blueprint, Vector2 tileLocation)
+        {
+            //buildingType = (int)
+            //humanDoor = new Point(-1, -1);
+            //animalDoor = new Point(-2, -1);
+            //indoors = null;
+            //nameOfIndoors = "";
+            //baseNameOfIndoors = "";
+            //nameOfIndoorsWithoutUnique = "";
+            //magical = false;
+            //tileX = 0;
+            //tileY = 0;
+            //maxOccupants = 0;
+            //tilesWide = 14;
+            //tilesHigh = 7;
+            //this.texture = content.Load<Texture2D>(@"assets\greenhouse.xnb", ContentSource.ModFolder);
+            //daysOfConstructionLeft = 1;
+            //leftShadow = new Rectangle(656, 394, 16, 16); //656, 394, 16, 16
+
+			this.tileX = (int)tileLocation.X;
+			this.tileY = (int)tileLocation.Y;
+			this.tilesWide = blueprint.tilesWidth;
+			this.tilesHigh = blueprint.tilesHeight;
+			this.buildingType = blueprint.name;
+			this.texture = blueprint.texture;
+			this.humanDoor = blueprint.humanDoor;
+			this.animalDoor = blueprint.animalDoor;
+			this.nameOfIndoors = blueprint.mapToWarpTo;
+			this.baseNameOfIndoors = this.nameOfIndoors;
+			this.nameOfIndoorsWithoutUnique = this.baseNameOfIndoors;
+			this.indoors = this.getIndoors();
+			this.nameOfIndoors = this.nameOfIndoors + (object)(this.tileX * 2000 + this.tileY);
+			this.maxOccupants = blueprint.maxOccupants;
+			this.daysOfConstructionLeft = 4;
+			this.magical = blueprint.magical;
+        }
+
+        public Greenhouse SetDaysOfConstructionLeft(int input)
+        {
+            daysOfConstructionLeft = input;
+            return this;
+        }
+
+        public override bool intersects(Rectangle boundingBox)
+        {
+            if (!base.intersects(boundingBox))
+                return false;
+            if (boundingBox.X >= (this.tileX + 4) * Game1.tileSize && boundingBox.Right < (this.tileX + 7) * Game1.tileSize)
+                return boundingBox.Y <= (this.tileY + 1) * Game1.tileSize;
+            return true;
+        }
+
+
+        public override void draw(SpriteBatch b)
+        {
+            if (this.daysOfConstructionLeft > 0)
+            {
+                this.drawInConstruction(b);
+            }
+            else
+            {
+                this.drawShadow(b, -1, -1);
+                b.Draw(this.texture, Game1.GlobalToLocal(Game1.viewport, new Vector2((float)(this.tileX * Game1.tileSize), (float)(this.tileY * Game1.tileSize + this.tilesHigh * Game1.tileSize))), new Rectangle?(this.texture.Bounds), this.color * this.alpha, 0.0f, new Vector2(0.0f, (float)this.texture.Bounds.Height), 4f, SpriteEffects.None, (float)((this.tileY + this.tilesHigh - 2) * Game1.tileSize) / 10000f);
+            }
+        }
+
+        public override void drawInMenu(SpriteBatch b, int x, int y)
+        {
+            CarpenterMenu menu = Game1.activeClickableMenu as CarpenterMenu;
+            float texScale = 2;
+            int num1 = (menu.maxWidthOfBuildingViewer - (int)(texture.Width * texScale)) / 2;
+            num1 -= (int)(texture.Width / 3.5);
+            int num2 = (menu.maxHeightOfBuildingViewer - (int)(texture.Height * texScale)) / 2;
+            this.drawShadow(b, num1, num2);
+            b.Draw(this.texture, new Rectangle(Game1.activeClickableMenu.xPositionOnScreen + num1, Game1.activeClickableMenu.yPositionOnScreen + num2, (int)(texture.Width * texScale), (int)(texture.Height * texScale)), Color.White);
+        }
+
+		public override void drawShadow(SpriteBatch b, int localX = -1, int localY = -1)
+		{
+			Vector2 position = localX == -1 ? Game1.GlobalToLocal(new Vector2((float)(this.tileX * Game1.tileSize), (float)((this.tileY + this.tilesHigh) * Game1.tileSize))) : new Vector2((float)localX, (float)(localY + this.getSourceRectForMenu().Height * Game1.pixelZoom));
+			b.Draw(Game1.mouseCursors, position, new Rectangle?(Building.leftShadow), Color.White * (localX == -1 ? this.alpha : 1f), 0.0f, Vector2.Zero, (float)Game1.pixelZoom, SpriteEffects.None, 1E-05f);
+			for (int index = 1; index < this.tilesWide - 1; ++index)
+				b.Draw(Game1.mouseCursors, position + new Vector2((float)(index * Game1.tileSize), 0.0f), new Rectangle?(Building.middleShadow), Color.White * (localX == -1 ? this.alpha : 1f), 0.0f, Vector2.Zero, (float)Game1.pixelZoom, SpriteEffects.None, 1E-05f);
+			b.Draw(Game1.mouseCursors, position + new Vector2((float)((this.tilesWide - 1) * Game1.tileSize), 0.0f), new Rectangle?(Building.rightShadow), Color.White * (localX == -1 ? this.alpha : 1f), 0.0f, Vector2.Zero, (float)Game1.pixelZoom, SpriteEffects.None, 1E-05f);
+		}
+
+        public override void dayUpdate(int dayOfMonth)
+        {
+            base.dayUpdate(dayOfMonth);
+            if (this.daysOfConstructionLeft > 0)
+                return;
+            //this.grabHorse();
+            //do special action for this building here
+            /*
+			 */
+        }
+
+        public override Rectangle getSourceRectForMenu()
+        {
+            return new Rectangle(0, 0, this.texture.Bounds.Width - 1, this.texture.Bounds.Height);
+        }
+    }
+}
---
+++ Stardewponics/Stardewponics.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Menus;
+using StardewValley.Buildings;
+using StardewValley.Characters;
+using StardewValley.TerrainFeatures;
+using StardewValley.Tools;
+using xTile;
+using xTile.Dimensions;
+using xTile.Tiles;
+using System.Collections.Generic;
+using StardewValley.Locations;
+using System.Linq;
+
+namespace Stardewponics
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /*********
+		** Properties
+		*********/
+        /// <summary>The tractor garage's building type.</summary>
+        private readonly string GarageBuildingType = "Aquaponics";
+
+        /// <summary>The tractor's NPC name.</summary>
+        //private readonly string TractorName = "Tractor";
+
+        /// <summary>The current player's farm.</summary>
+        private Farm Farm;
+
+        private bool IsNewDay;
+
+        /*********
+		** Public methods
+		*********/
+        /// <summary>Initialise the mod.</summary>
+        /// <param name="helper">Provides methods for interacting with the mod directory, such as read/writing a config file or custom JSON files.</param>
+        public override void Entry(IModHelper helper)
+        {
+            ControlEvents.KeyPressed += this.ReceiveKeyPress;
+            MenuEvents.MenuChanged += this.MenuAddInBuilding;
+
+            // spawn tractor & remove it before save
+            TimeEvents.AfterDayStarted += this.TimeEvents_AfterDayStarted;
+            //LocationEvents.CurrentLocationChanged += this.LocationEvents_CurrentLocationChanged;
+            SaveEvents.BeforeSave += this.SaveEvents_BeforeSave;
+
+        }
+
+
+		/*********
+		** Private methods
+		*********/
+		/// <summary>The method invoked when the player presses a keyboard button.</summary>
+		/// <param name="sender">The event sender.</param>
+		/// <param name="e">The event data.</param>
+
+//		private void LocationEvents_CurrentLocationChanged(object sender, EventArgsCurrentLocationChanged e)
+		private void LocationEvents_CurrentLocationChanged(object sender, EventArgs eventArgs)
+        {
+
+			// spawn tractor house & tractor
+			//			if (this.IsNewDay && e.NewLocation == this.Farm)
+			if (this.IsNewDay)
+
+			{
+                this.RestoreCustomData();
+                this.IsNewDay = false;
+            }
+        }
+
+        private void SaveEvents_BeforeSave(object sender, EventArgs eventArgs)
+        {
+            this.StashCustomData();
+        }
+
+
+        /// <summary>Stash all tractor and garage data to a separate file to avoid breaking the save file.</summary>
+        private void StashCustomData()
+        {
+            // back up garages
+            Building[] garages = this.GetGreenhouses(this.Farm).ToArray();
+            CustomSaveData saveData = new CustomSaveData(garages);
+            this.Helper.WriteJsonFile($"data/{Constants.SaveFolderName}.json", saveData);
+
+            // remove tractors + buildings
+            foreach (Building garage in garages)
+                this.Farm.destroyStructure(garage);
+            //foreach (GameLocation location in Game1.locations)
+            //	this.RemoveEveryCharactersOfType<Tractor>(location);
+        }
+
+
+        /// <summary>Restore tractor and garage data removed by <see cref="StashCustomData"/>.</summary>
+        private void RestoreCustomData()
+        {
+            Monitor.Log("RestoreCustomData");
+            // get save data
+            CustomSaveData saveData = this.Helper.ReadJsonFile<CustomSaveData>($"data/{Constants.SaveFolderName}.json");
+
+            if (saveData?.Buildings == null)
+                return;
+
+            Monitor.Log("Begin Adding");
+            // add garages
+            BluePrint blueprint = this.CreateGreenhouse();
+            foreach (CustomSaveBuilding building in saveData.Buildings)
+            {
+                Building newGarage = new Greenhouse(blueprint, building.Tile)
+                {
+                    buildingType = this.GarageBuildingType,
+                    daysOfConstructionLeft = building.DaysOfConstructionLeft
+                }; // rebuild to avoid data issues
+                Monitor.Log("building " + newGarage.tileX.ToString() );
+                this.Farm.buildings.Add(newGarage);
+
+
+				if (this.Farm.isThereABuildingUnderConstruction() && this.Farm.getBuildingUnderConstruction().daysOfConstructionLeft > 0 && Game1.getCharacterFromName("Robin", false).currentLocation.Equals((object)this))
+				{
+					Building underConstruction = this.Farm.getBuildingUnderConstruction();
+					this.Farm.temporarySprites.Add(new TemporaryAnimatedSprite(Game1.mouseCursors, new Microsoft.Xna.Framework.Rectangle(399, 262, underConstruction.daysOfConstructionLeft == 1 ? 29 : 9, 43), new Vector2((float)(underConstruction.tileX + underConstruction.tilesWide / 2), (float)(underConstruction.tileY + underConstruction.tilesHigh / 2)) * (float)Game1.tileSize + new Vector2((float)(-Game1.tileSize / 4), (float)(-Game1.tileSize * 2 - Game1.tileSize / 4)), false, 0.0f, Color.White)
+					{
+						scale = (float)Game1.pixelZoom,
+						interval = 999999f,
+						animationLength = 1,
+						totalNumberOfLoops = 99999,
+						layerDepth = (float)((double)((underConstruction.tileY + underConstruction.tilesHigh / 2) * Game1.tileSize + Game1.tileSize / 2) / 10000.0)
+					});
+				}
+
+                //this.Farm.buildStructure(newGarage, building.Tile, false, Game1.player);
+                //if (this.IsNewTractor)
+                //	this.SpawnTractor();
+            }
+        }
+
+
+
+        private void ReceiveKeyPress(object sender, EventArgsKeyPressed e)
+        {
+            if (e.KeyPressed == Keys.OemCloseBrackets)
+            {
+
+                //int start = 30;
+                //int farmX = 25;
+                //int farmY = 40;
+                //this.Monitor.Log("Build Greenhouse key pressed. ]");
+                //this.Farm.buildStructure(new Greenhouse(this.Helper.Content).SetDaysOfConstructionLeft(0), new Vector2(farmX, farmY), false, Game1.player);
+
+
+
+                //GameLocation farmLocation = Game1.getLocationFromName("Farm");
+
+                //farmLocation.warps.Add(new Warp(farmX + 4, farmY + 4, "Greenhouse", start + 10, start + 23, false));
+                //farmLocation.warps.Add(new Warp(farmX + 5, farmY + 4, "Greenhouse", start + 10, start + 23, false));
+
+                ////Game1.locations[1].Name
+
+                //GameLocation greenhouseLocation = Game1.getLocationFromName("Greenhouse");
+                //var tilesheet = greenhouseLocation.map.GetTileSheet("untitled tile sheet");
+
+
+                //var aquaponics = this.Helper.Content.Load<Map>(@"assets\greenhousemap.xnb", ContentSource.ModFolder);
+                //var layers = new[] { "Back", "Buildings", "Front" };
+                //foreach (string lay in layers)
+                //{
+                //var aqualayer = aquaponics.GetLayer(lay);
+                //var layer = greenhouseLocation.map.GetLayer(lay);
+                //layer.LayerSize = new xTile.Dimensions.Size(230, 230);
+
+                //	for (int x = 0; x<aqualayer.LayerSize.Width; x++)
+                //	{
+                //		for (int y = 0; y<aqualayer.LayerSize.Height; y++)
+                //		{
+                //			var aquaTile = aqualayer.Tiles[x, y];
+                //			if (aquaTile != null)
+                //				layer.Tiles[start + x, start + y] = new StaticTile(layer, tilesheet, BlendMode.Alpha, aquaTile.TileIndex);
+                //		}
+                //	}
+                //}
+
+                //greenhouseLocation.warps.Add(new Warp(start + 10, start + 24, "Farm", farmX + 4, farmY + 7, false));
+            }
+
+        }
+
+        private void TimeEvents_AfterDayStarted(object sender, EventArgs eventArgs)
+        {
+            this.IsNewDay = true;
+            this.Farm = Game1.getFarm();
+			this.RestoreCustomData();
+        }
+
+        private void MenuForceOurBuildingRendering(object sender, EventArgs eventArgs)
+        {
+            var carpenter = Game1.activeClickableMenu as CarpenterMenu;
+            if (carpenter == null)
+            {
+                GameEvents.UpdateTick -= MenuForceOurBuildingRendering;
+                return;
+            }
+
+            var currBuildingField = Helper.Reflection.GetPrivateField<Building>(carpenter, "currentBuilding");
+            var currBuilding = currBuildingField.GetValue();
+            if (currBuilding is Building && currBuilding.buildingType == this.GarageBuildingType)
+            {
+                currBuildingField.SetValue(new Greenhouse(CreateGreenhouse(), new Vector2()));
+            }
+        }
+
+        private void MenuAddInBuilding(object sender, EventArgsClickableMenuChanged e)
+        {
+
+            if (e.NewMenu is CarpenterMenu)
+            {
+                GameEvents.UpdateTick += MenuForceOurBuildingRendering;
+
+                List<BluePrint> blueprints = this.Helper.Reflection.GetPrivateValue<List<BluePrint>>(Game1.activeClickableMenu, "blueprints");
+                blueprints.Add(CreateGreenhouse());
+            }
+
+
+        }
+
+        private BluePrint CreateGreenhouse()
+        {
+            BluePrint AquaBP = new BluePrint(GarageBuildingType);
+            AquaBP.itemsRequired.Clear();
+
+            string[] strArray2 = "390 200".Split(' ');
+            int index = 0;
+            while (index < strArray2.Length)
+            {
+                if (!strArray2[index].Equals(""))
+                    AquaBP.itemsRequired.Add(Convert.ToInt32(strArray2[index]), Convert.ToInt32(strArray2[index + 1]));
+                index += 2;
+            }
+            AquaBP.texture = this.Helper.Content.Load<Texture2D>(@"assets\greenhouse.xnb", ContentSource.ModFolder);
+            AquaBP.humanDoor = new Point(-1, -1);
+            AquaBP.animalDoor = new Point(-2, -1);
+            AquaBP.mapToWarpTo = "null";
+            AquaBP.displayName = "Aquaponics";
+            AquaBP.description = "A place to grow plants using fertilized water from your Fish!";
+            AquaBP.blueprintType = "Buildings";
+            AquaBP.nameOfBuildingToUpgrade = "";
+            AquaBP.actionBehavior = "null";
+            AquaBP.maxOccupants = -1;
+            AquaBP.moneyRequired = 100; //ModConfig.TractorHousePrice;
+            AquaBP.tilesWidth = 14;
+            AquaBP.tilesHeight = 7;
+            AquaBP.getTileSheetIndexForStructurePlacementTile(0, 0);
+            AquaBP.sourceRectForMenuView = new Microsoft.Xna.Framework.Rectangle(0, 0, 96, 96);
+            AquaBP.namesOfOkayBuildingLocations.Clear();
+            AquaBP.namesOfOkayBuildingLocations.Add("Farm");
+            AquaBP.magical = false;
+
+            return AquaBP;
+        }
+
+        /// <summary>Get all garages in the given location.</summary>
+        /// <param name="location">The location to search.</param>
+        private IEnumerable<Building> GetGreenhouses(BuildableGameLocation location)
+        {
+            return location.buildings.Where(building => building.buildingType == this.GarageBuildingType);
+        }
+    }
+}
